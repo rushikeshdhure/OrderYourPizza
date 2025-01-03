9fe72186c46a15e1ed1bc8b38fa9c4c743a4e94b
Rushikesh  Dhure <rushikeshdhure@example.com>
2025-01-03 20:58:50 +0530

    models and views are deleted.

--- pizzaplace/app/migrations/0001_initial.py
+++ pizzaplace/app/migrations/0001_initial.py
@@ -8,25 +8,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
-    operations = [
-        migrations.CreateModel(
-            name='UserRegister',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('email', models.EmailField(max_length=254, unique=True)),
-                ('password', models.CharField(max_length=128)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Token',
-            fields=[
-                ('key', models.CharField(max_length=40, primary_key=True, serialize=False)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='auth_token', to='app.userregister')),
-            ],
-        ),
-    ]
+    operations = []
--- pizzaplace/app/urls.py
+++ pizzaplace/app/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import index_view, RegisterView, LoginView, LogoutView, ProtectedView
+from .views import index_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('protected/', ProtectedView.as_view(), name='protected'),
+
 ]
