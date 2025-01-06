d23482a4ad021ed8a4c5a8c0d693ccfcb65d5b2c
Soham kamble <s2ksoham@gmail.com>
2025-01-06 23:26:28 +0530

    test

--- pizzaplace/app/urls.py
+++ pizzaplace/app/urls.py
@@ -4,5 +4,6 @@ from .views import index_view, RegisterView
 urlpatterns = [
     path('', index_view, name='index'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', name='login')
 
 ]
