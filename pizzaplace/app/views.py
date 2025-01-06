c6dc7d181bfdbc6cd51b68d301a36e0c0d81cfc6
Rushikesh  Dhure <rushikeshdhure@example.com>
2025-01-06 23:20:05 +0530

    Login Feature added.

--- pizzaplace/app/views.py
+++ pizzaplace/app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer
 from rest_framework.response import Response
-
+from django.core.exceptions import ValidationError
+from app.models import Register
 def index_view(request):
     return render(request, 'index.html')
+
 class RegisterView(APIView):
     template_name = 'register.html'
 
@@ -20,8 +22,34 @@ class RegisterView(APIView):
         serializer = RegisterSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return redirect('login.html')
+            return redirect('login')
         
         return render(request, self.template_name, {'errors': serializer.errors})
-    
 
+
+class LoginView(APIView):
+    template_name = 'login.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.POST)
+        
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            # Store user information in session
+            request.session['user_id'] = user.id
+            request.session['user_email'] = user.email
+            request.session['user_name'] = user.name
+            
+            # Handle remember me checkbox
+            if request.POST.get('checkbox'):
+                request.session.set_expiry(1209600)  # 2 weeks
+            else:
+                request.session.set_expiry(0)  # Until browser closes
+
+            return redirect('index')  # Replace 'home' with your dashboard URL
+        
+        # If validation fails, render the template with errors
+        return render(request, self.template_name, {'errors': serializer.errors})
