f29dfca397128d5db44e30ece7f6b222bfdd1b74
Rushikesh  Dhure <rushikeshdhure@example.com>
2025-01-06 20:54:21 +0530

    settings.py added to gitignore

--- pizzaplace/app/views.py
+++ pizzaplace/app/views.py
@@ -5,21 +5,6 @@ from rest_framework.response import Response
 
 def index_view(request):
     return render(request, 'index.html')
-
-# def register_view(request):
-#     return render(request, 'register.html')
-
-# class RegisterView(APIView):
-#     def post(self, request):
-#         serializer = RegisterSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-        
-#         return Response({
-#             'message' : 'User registered successfully',
-#             'register' : serializer.data
-#         })
-
 class RegisterView(APIView):
     template_name = 'register.html'
 
@@ -38,3 +23,5 @@ class RegisterView(APIView):
             return redirect('login.html')
         
         return render(request, self.template_name, {'errors': serializer.errors})
+    
+
